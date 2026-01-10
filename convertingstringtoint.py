87ad4402ab8fec5ccc8ce51237651d8bec2ec980
C R <s.c.r@accenture.com>
2026-01-10 21:06:24 +0530

    testing if i can commit

---
+++ convertingstringtoint.py
@@ -0,0 +1,4 @@
+# in this script i am using input function, and converting the input to integer
+uss=input('What is US foot meausurement?')
+uks=int(uss) - 1 #converting the input string to integer and using operator
+print("UK footware measurement is, ", uks) 
